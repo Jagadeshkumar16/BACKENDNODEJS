586be275d00bb006cfe417ea2edf01ce767cf67b
jagadesh kumar <jagadeshk1607@gmail.com>
2024-03-11 19:48:58 +0530

    base setup

---
+++ app/controllers/organism.controller.js
@@ -0,0 +1,143 @@
+const db = require("../models");
+const Organism = db.organism;
+
+// Create and Save a new Organism
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.species) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  const organism = new Organism({
+    species: req.body.species,
+    age: req.body.age,
+    health: req.body.health,
+    environment: req.body.environment
+  });
+
+  organism
+    .save(organism)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Organism."
+      });
+    });
+};
+
+// Retrieve all Organisms from the database.
+exports.findAll = (req, res) => {
+  console.log("testing");
+  const species = req.query.species;
+  var condition = species ? { species: { $regex: new RegExp(species), $options: "i" } } : {};
+
+  Organism.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving organisms."
+      });
+    });
+};
+
+// Find a single Organism with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Organism.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Organism with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Organism with id=" + id });
+    });
+};
+
+// Update a Organism by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Organism.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Oraganism with id=${id}. Maybe Organism was not found!`
+        });
+      } else res.send({ message: "Organism was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Organism with id=" + id
+      });
+    });
+};
+
+// Delete a Organism with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Organism.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Organism with id=${id}. Maybe Organism was not found!`
+        });
+      } else {
+        res.send({
+          message: "Organism was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Organism with id=" + id
+      });
+    });
+};
+
+// Delete all Organisms from the database.
+exports.deleteAll = (req, res) => {
+  Organism.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Organisms were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all organisms."
+      });
+    });
+};
+
+// Find all published Organisms
+exports.findAllPublished = (req, res) => {
+  Organism.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving organisms."
+      });
+    });
+};
